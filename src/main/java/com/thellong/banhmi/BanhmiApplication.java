ecaea1b3a6eea6c0a4c789ce8b40e4f307c3b7e0
thellong <firaxaxa@gmail.com>
2022-09-10 19:38:25 +0700

    Initialize project

---
+++ src/main/java/com/thellong/banhmi/BanhmiApplication.java
@@ -0,0 +1,13 @@
+package com.thellong.banhmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanhmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanhmiApplication.class, args);
+	}
+
+}
