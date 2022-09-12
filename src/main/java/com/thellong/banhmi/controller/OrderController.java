8753fff092b84a2ba1095f89e1bffc2054a3e2ff
thellong <firaxaxa@gmail.com>
2022-09-12 23:30:56 +0700

    Order model Controller

---
+++ src/main/java/com/thellong/banhmi/controller/OrderController.java
@@ -0,0 +1,18 @@
+package com.thellong.banhmi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.thellong.banhmi.model.Order;
+
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+}
