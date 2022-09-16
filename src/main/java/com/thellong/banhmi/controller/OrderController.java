618faedd94a5822a63600d7996046be71f688bd5
thellong <firaxaxa@gmail.com>
2022-09-16 23:36:38 +0700

    getting order info from form

--- src/main/java/com/thellong/banhmi/controller/OrderController.java
+++ src/main/java/com/thellong/banhmi/controller/OrderController.java
@@ -3,10 +3,14 @@ package com.thellong.banhmi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.thellong.banhmi.model.Order;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
@@ -14,5 +18,11 @@ public class OrderController {
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order) {
+        log.info("Order submitted: " + order);
+         return "redirect:/";
     }
 }
--- src/main/java/com/thellong/banhmi/model/Order.java
+++ src/main/java/com/thellong/banhmi/model/Order.java
@@ -1,5 +1,15 @@
 package com.thellong.banhmi.model;
 
+import lombok.Data;
+
+@Data
 public class Order {
-    
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
 }
